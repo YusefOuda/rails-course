2795058407ddd596d4dea81cfda3fafbacd47d3a
Yusef Ouda <yiouda92@gmail.com>
2014-10-14 11:13:45 -0500

    Add 2 new routes for edit and put (for videos). They will allow videos to be updated

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Metube::Application.routes.draw do
   get "/videos", to: "videos#index"
   get "/videos/new", to: "videos#new"
   get "/videos/:id", to: "videos#show"
-
   post "/videos" => "videos#create"
+  get "/videos/:id/edit", to: ":videos#edit"
+  put "/videos/:id", to: "videos#update"
 
 
   # The priority is based upon order of creation: first created -> highest priority.
