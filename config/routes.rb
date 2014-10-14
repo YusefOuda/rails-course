6da1b2ab6c00781b12a552bc65f35d8c54ce58f7
Yusef Ouda <yiouda92@gmail.com>
2014-10-14 11:27:56 -0500

    Change routes.rb file to use resources instead of explicitly writing seperate routes out

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Metube::Application.routes.draw do
   root to: "videos#index"
-  get "/videos", to: "videos#index"
-  get "/videos/new", to: "videos#new"
-  get "/videos/:id", to: "videos#show"
-  post "/videos" => "videos#create"
-  get "/videos/:id/edit", to: "videos#edit"
-  put "/videos/:id", to: "videos#update"
-  delete '/videos/:id' => 'videos#destroy'
+  resources :videos
 
 
   # The priority is based upon order of creation: first created -> highest priority.
